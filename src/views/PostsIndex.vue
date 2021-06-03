842b06c502ed92103452d4f67bde6b6321ff29f9
jeffstienstra <jeffstienstra@gmail.com>
2021-06-02 22:27:24 -0400

    initial commit

---
+++ src/views/PostsIndex.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>All Blog Posts</h1>
+    <p>{{ posts }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      posts: {},
+    };
+  },
+  created: function () {
+    this.postsIndex();
+  },
+  methods: {
+    postsIndex: function () {
+      console.log("blog posts");
+    },
+  },
+};
+</script>
